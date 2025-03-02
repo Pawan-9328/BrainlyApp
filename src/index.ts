6afa20ab32d371be03badc96d7ab6fd1265d8a4e
pawan <golapawan941@gmail.com>
2025-03-03 02:39:35 +0530

    Set the branApp

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express"; 
+import mongoose from "mongoose";
+
+
+
+const app = express();
+
+
+
+app.post("/api/v1/signup" , (req, res) =>{
+
+
+})
+
+app.post("/api/v1/signin" , (req, res) =>{
+
+
+})
+
+app.post("/api/v1/content" , (req, res) =>{
+
+
+})
+
