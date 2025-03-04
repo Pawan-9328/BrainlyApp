36b0cb3eb265a194b26dff3c659bf339da904664
pawan <golapawan941@gmail.com>
2025-03-04 18:07:05 +0530

    content db done

--- src/db.ts
+++ src/db.ts
@@ -11,3 +11,12 @@ mongoose.connect('mongodb://localhost:27017/brainlyDB')
 
 export const UserModel =  model("User",UserSchema); 
 
+
+const ContentSchema = new Schema({
+     title: String,
+     link: String,
+     tags: {type : mongoose.Types.ObjectId, ref: 'Tag'},
+     userId: {type : mongoose.Types.ObjectId, ref: 'User', required: true}
+})
+
+export const ContentModel = model("Content", ContentSchema);
