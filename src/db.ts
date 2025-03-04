0d08cb14af63c337e510f30c71ddb20a85718849
pawan <golapawan941@gmail.com>
2025-03-04 17:59:03 +0530

    write user signup and signin both

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,10 @@
-import {model, Schema } from "mongoose";
+import mongoose, {model, Schema } from "mongoose";
 
-const UserSchema = new Schema({
+mongoose.connect('mongodb://localhost:27017/brainlyDB')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+    const UserSchema = new Schema({
          username : {type: String, unique: true},
          password: String
 })
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,66 @@
 import express from "express"; 
-import mongoose from "mongoose";
+import  jwt  from "jsonwebtoken";
+import { UserModel } from "./db"; 
 
 
+
+const JWT_PASSWORD = "!23123"; 
 
 const app = express();
+app.use(express.json()); 
 
 
+app.post("/api/v1/signup" , async (req, res) =>{
+ const username = req.body.username;
+ const password = req.body.password;
 
-app.post("/api/v1/signup" , (req, res) =>{
- 
-
+  try {
+    await UserModel.create({
+        username: username,
+        password: password
+    })
+    res.json({
+        message: "User signed up"
+    })
+      
+  } catch(e) {
+       res.json(411).json({
+        message: "User already exists"
+       })
+  }
 })
 
-app.post("/api/v1/signin" , (req, res) =>{
+app.post("/api/v1/signin" ,async (req, res) =>{
+  const username = req.body.username;
+  const password = req.body.password;
+  const existingUser = await UserModel.findOne({
+     username,
+     password
+  })
+
+  if(existingUser) {
+     const token = jwt.sign({
+         id: existingUser.id
+     }, JWT_PASSWORD)
+
+      res.json({
+         token 
+      })
+  } else{
+       res.status(403).json({
+         message : "Incorrect credentials"
+       })
+  }
 
 
+ 
 })
 
 app.post("/api/v1/content" , (req, res) =>{
 
 
 })
+
+
+app.listen(3000); 
 
